bde66ffb41d002cce9af1131be2fefb15670adca
sourabh kumar singh <sss.sourabh.7@gmail.com>
2023-03-01 21:50:49 +0530

    pushing kantega first time

---
+++ target/classes/public/precache-manifest.a9685e35188c94ce315442e7b9b3e318.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "771ec94b1d5ef7ac955bc402fe56d305",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7dbe2e3b95c4b842e5f",
+    "url": "/static/css/main.4b738920.chunk.css"
+  },
+  {
+    "revision": "531b36856d98405b28f4",
+    "url": "/static/js/2.5e0613e2.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/static/js/2.5e0613e2.chunk.js.LICENSE"
+  },
+  {
+    "revision": "d7dbe2e3b95c4b842e5f",
+    "url": "/static/js/main.f8787b67.chunk.js"
+  },
+  {
+    "revision": "0281e3aa3ea9fc02c242",
+    "url": "/static/js/runtime-main.b12ff581.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
